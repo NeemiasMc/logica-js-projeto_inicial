06f1b6b199d54774aa94c27daa684ed894fd91cb
neemias mateus ferreira de campos <zeuztrovao959@gmail.com>
2024-01-17 20:57:56 -0300

    aprendo java

---
+++ app.js
@@ -0,0 +1,8 @@
+
+alert('boas vinda ao jogodo numero secreto');
+let numeroSecreto = 5 ;
+let chute = prompt('escolha um numero entre 1 e 10');
+
+if(chute == numeroSecreto){
+    console.log('isso ai ! voce descrobriu o numero secreto (5)');
+}
---
+++ desafio;pratico/ex.js
@@ -0,0 +1,33 @@
+//#endregion desafio 1
+alert('boas vindas ao nosso site!');
+
+//desafio 2
+let nome1 = 'Lua';
+
+// desafio 3
+let idade1 = 25;
+//desafio 4
+let numeroDeVendas = 50;
+
+//desafio 5 
+let saldoDisponivel = 1000;
+
+// desafio 6 
+alert('erro!preencha todos os campos');
+
+//desafio 7
+let mesagemDeErro = "erro! preencha todos os campos";
+alert(mensagemDeErro);
+
+//desafio 8
+let nome = prompt ('digite seu nome');
+
+//desafio 9 
+let idade = prompt ('digite sua idade!');
+
+//desafio 10 
+let maior = prompt('digite sua idade!')
+
+if(25 >= 18){
+    alert('pode tirar a habilitacao!')
+}
